e67661c99ba168fff18b04ab657175629a873d12
HungTuLenh <ngduyhung811@gmail.com>
2025-01-06 00:38:59 +0700

    new: component, jsx, DOM event

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import MyComponent from './Example/MyComponent';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rì ách hello world
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent/>
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+class MyComponent extends React.Component{
+
+    state = {
+        name: 'SigmaTodo',
+        master: 'HungTuLenh'
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState(
+            {
+                name: event.target.value
+            }
+        )
+    }
+    
+    handleOnClickAddTodo = () => {
+        alert("Thêm thành công")
+    }
+
+    render(){
+        return(
+            <>
+                <div className = '1'>Tên tôi là: {this.state.name}</div>
+                <div className = '2'>
+                    <input type = "text" value = {this.state.name}
+                    onChange = {(event) => this.handleOnChangeName(event)}
+                    />
+                </div>
+                <div className = '3'>
+                    Được tạo ra bởi {this.state.master}
+                </div>
+                <div className = '4'>
+                    <button onClick = {() => this.handleOnClickAddTodo()}>
+                        Thêm việc cần làm
+                    </button>
+                </div>
+            </>
+            
+        )
+    }
+
+
+}
+
+export default MyComponent;
